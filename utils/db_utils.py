6c9e090ba4e3bbb035c5ab2ce66ca791f365f4e3
Kito <kito.tran@reginamiracle.com>
2025-03-31 10:34:22 +0700

    Add cryptography utility and update database connection string in db_utils.py

--- utils/db_utils.py
+++ utils/db_utils.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 
 def get_db_connection():
     # Database connection function for common use
-    conn_str = "Driver={ODBC Driver 17 for SQL Server};Server=172.19.18.58;Database=Regina_User;UID=ros;PWD=*123qwe*;Encrypt=YES;TrustServerCertificate=YES;"
+    conn_str = "Driver={ODBC Driver 17 for SQL Server};Server=172.19.18.58;Database=AIDB;UID=ros;PWD=*123qwe*;Encrypt=YES;TrustServerCertificate=YES;"
     try:
         conn = pyodbc.connect(conn_str)
         return conn
